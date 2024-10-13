f219cad0a6cd2bae2edaafab43c8e2e5988d500d
Jerry Hobby <jerry@jerryhobby.com>
2024-10-12 22:53:52 -0500

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Godot 2.5D RPG Course
+
+The book: https://luisramirez.gitbook.io/godot-2.5d-rpg-course
+
+## Godot C# Reference
+
+https://docs.godotengine.org/en/stable/classes/index.html
---
+++ Scripts/Characters/Player/Player.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody3D
+{
+    public int Speed = 1;
+    public override void _PhysicsProcess(double delta)
+    {
+        //base._PhysicsProcess(delta);
+
+        //GD.Print("Player _PhysicsProcess");
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        Vector2 moveDirection;
+        moveDirection = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward");
+
+        GD.Print("Player _Input", moveDirection);
+
+        Velocity = new Vector3(moveDirection.X, 0, moveDirection.Y);
+
+        MoveAndCollide(Velocity * Speed);
+
+    }
+}
